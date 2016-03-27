88e5f937cec77e2cff4dcca9277009a73cc77066
Sergio <sdmoralesma@gmail.com>
2016-03-27 12:39:01 -0500

    first integration primeng tree

--- app/filemanager/filemanager.component.ts
+++ app/filemanager/filemanager.component.ts
@@ -1,9 +1,47 @@
 import {Component} from "angular2/core";
+import {Tree, TreeNode, Message} from "primeng/primeng";
 
 @Component({
     selector: 'filemanager',
-    templateUrl: './app/filemanager/filemanager.html'
+    templateUrl: './app/filemanager/filemanager.html',
+    directives: [Tree]
 })
 export class FileManagerComponent {
+
+    msgs: Message[];
+
+    files: TreeNode[];
+
+    selectedFile: TreeNode;
+
+    constructor() { }
+
+    ngOnInit() {
+        var myNode: MyNode;
+        myNode = {
+            label: "string",
+            data: "any",
+            icon: "any",
+            expandedIcon: "any",
+            collapsedIcon: "any",
+            leaf: true
+        };
+
+        this.files = [myNode];
+    }
+
+    nodeSelect(event) {
+        this.msgs = [];
+        this.msgs.push({severity: 'info', summary: 'Node Selected', detail: event.node.label});
+    }
+
+    nodeUnselect(event) {
+        this.msgs = [];
+        this.msgs.push({severity: 'info', summary: 'Node Unselected', detail: event.node.label});
+    }
+
+}
+
+class MyNode implements TreeNode {
 
 }
