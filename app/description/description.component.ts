1dae892845e665ae55cb4463b31fda697b0a3d22
Sergio Morales <sergio.morales2@wnco.com>
2016-04-07 21:46:04 -0500

    description editor added

--- app/description/description.component.ts
+++ app/description/description.component.ts
@@ -1,9 +1,11 @@
 import {Component} from "angular2/core";
+import {Editor, Header} from "primeng/primeng";
 
 @Component({
     selector: 'description',
-    templateUrl: './app/description/description.html'
+    templateUrl: './app/description/description.html',
+    directives: [Editor, Header]
 })
 export class DescriptionComponent {
-
+    descriptionText: string = "hello my fucking world!";
 }
