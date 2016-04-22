845d5a3b990139f70ece3e27a60867e6ed5f76e1
Sergio <sdmoralesma@gmail.com>
2016-04-21 22:34:32 -0500

    fixed splitButton Error.

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,8 @@
 import {bootstrap} from "angular2/platform/browser";
 import {AppComponent} from "./app.component";
+import {APP_BASE_HREF, ROUTER_PROVIDERS} from "angular2/router";
+import {provide} from "angular2/core";
 
-bootstrap(AppComponent,[]).catch(err => console.error(err));
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    provide(APP_BASE_HREF, {useValue: '/'})]).catch(err => console.error(err));
