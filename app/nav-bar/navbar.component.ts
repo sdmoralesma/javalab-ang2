905db02d9efdc71be9e9d2ea507d09dae03b5f99
Sergio <sdmoralesma@gmail.com>
2016-05-11 22:46:56 +0200

    removed resize function, ruler, rectangle and browser -> upgraded to angular RC1

--- app/nav-bar/navbar.component.ts
+++ app/nav-bar/navbar.component.ts
@@ -1,8 +1,5 @@
-import {Component, ElementRef, OnInit} from "angular2/core";
-import {Ruler, Rectangle} from "angular2/src/platform/browser/ruler";
-import * as browser from "angular2/platform/browser";
+import {Component, ElementRef, OnInit} from "@angular/core";
 import {AutoComplete, Menubar, Button, SplitButton, SplitButtonItem} from "primeng/primeng";
-
 
 @Component({
     selector: 'nav-bar',
@@ -13,44 +10,22 @@ import {AutoComplete, Menubar, Button, SplitButton, SplitButtonItem} from "prime
 export class NavBarComponent implements OnInit {
     height:number;
     element:ElementRef;
-    ruler:Ruler;
-    rect:any;
 
     constructor(element:ElementRef) {
         this.element = element;
-        this.ruler = new Ruler(new browser.BrowserDomAdapter());
-        this.rect = {};
     }
 
     ngOnInit() {
         var vm = this;
-        var measure = this.ruler.measure(this.element);
-        measure.then((rect:Rectangle) => {
-            this.height = rect.height;
-            vm.rect = rect;
-        });
-
         this.text = "my testing text";
     }
-
-    onResize(rect:Rectangle) {
-        var vm = this;
-        var measure = this.ruler.measure(this.element);
-        measure.then((rect:Rectangle) => {
-            this.height = rect.height;
-            vm.rect = rect;
-        });
-    }
-
 
     text:string;
 
     results:string[];
 
     search(event) {
-
         this.results = ["asdf", "qwerty"];
-
     }
 
     handleDropdown(event) {
