0bb3851dac30c694d2e30496cceaa1f9be0f2a1f
Sergio <sdmoralesma@gmail.com>
2016-05-23 00:54:22 +0200

    added ids to nodes

--- app/mock-response.ts
+++ app/mock-response.ts
@@ -1,41 +1,51 @@
 export var MockResponse:any = {
     "console": "Welcome to Javalab v0.3.0 !\r\n$ java -version : 1.8.0_66 Java HotSpot(TM) 64-Bit Server VM",
-    "treedata": [{
-        "label": "dependencies",
-        "icon": "fa-file-text-o",
-        "data": "testCompile 'junit:junit:+'\n",
-        "children": []
-    }, {
-        "label": "src/main/java/",
-        "expandedIcon": "fa-folder-open",
-        "collapsedIcon": "fa-folder",
-        "children": [{
-            "label": "com.company.project",
-            "icon": "fa fa-th-large",
+    "filesTree": [
+        {
+            "id": 1,
+            "label": "dependencies",
+            "icon": "fa-file-text-o",
+            "data": "testCompile 'junit:junit:+'\n",
+            "children": []
+        },
+        {
+            "id": 2,
+            "label": "src/main/java/",
+            "expandedIcon": "fa-folder-open",
+            "collapsedIcon": "fa-folder",
             "children": [{
-                "label": "HelloWorld.java",
-                "icon": "fa-file-text-o",
-                "data": "package com.company.project;\n\npublic class HelloWorld {\n\n    public static void main(String[] args) {\n        System.out.println(\"hello world java!\");\n    }\n\n}\n",
-                "cursor": "",
-                "children": []
+                "label": "com.company.project",
+                "icon": "fa fa-th-large",
+                "children": [{
+                    "id": 21,
+                    "label": "HelloWorld.java",
+                    "icon": "fa-file-text-o",
+                    "data": "package com.company.project;\n\npublic class HelloWorld {\n\n    public static void main(String[] args) {\n        System.out.println(\"hello world java!\");\n    }\n\n}\n",
+                    "cursor": "",
+                    "children": []
+                }]
             }]
-        }]
-    }, {
-        "label": "src/test/java/",
-        "expandedIcon": "fa-folder-open",
-        "collapsedIcon": "fa-folder",
-        "children": [{
-            "label": "com.company.project",
-            "icon": "fa fa-th-large",
+        },
+        {
+            "id": 3,
+            "label": "src/test/java/",
+            "expandedIcon": "fa-folder-open",
+            "collapsedIcon": "fa-folder",
             "children": [{
-                "label": "HelloWorldTest.java",
-                "icon": "fa-file-text-o",
-                "data": "package com.company.project;\n\nimport org.junit.*;\n\npublic class HelloWorldTest {\n\n    @Test\n    public void test() {\n        Assert.assertEquals(1, 1);\n    }\n}\n",
-                "cursor": "",
-                "children": []
+                "id": 31,
+                "label": "com.company.project",
+                "icon": "fa fa-th-large",
+                "children": [{
+                    "id": 311,
+                    "label": "HelloWorldTest.java",
+                    "icon": "fa-file-text-o",
+                    "data": "package com.company.project;\n\nimport org.junit.*;\n\npublic class HelloWorldTest {\n\n    @Test\n    public void test() {\n        Assert.assertEquals(1, 1);\n    }\n}\n",
+                    "cursor": "",
+                    "children": []
+                }]
             }]
-        }]
-    }],
+        }
+    ],
     "runnableNode": {"mainClass": false, "testClass": false},
     "initConfig": {
         "language": "java",
