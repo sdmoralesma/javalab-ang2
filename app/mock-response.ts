9b48b595b997ed46ef990cb0e738c2972ea6d3c9
Sergio <sdmoralesma@gmail.com>
2016-05-28 18:17:07 +0200

    fixed property

--- app/mock-response.ts
+++ app/mock-response.ts
@@ -17,14 +17,14 @@ export var MockResponse:any = {
                 "id": "71c8eed2-9732-45b4-8299-5eca126bdce3",
                 "label": "com.company.project",
                 "icon": "fa fa-th-large",
-                "parent": "e0549dae-1e0b-4819-8ccb-1ea3d790b7ae",
+                "parentId": "e0549dae-1e0b-4819-8ccb-1ea3d790b7ae",
                 "children": [{
                     "id": "e597e833-8f97-4a13-b119-71a3b093aecc",
                     "label": "HelloWorld.java",
                     "icon": "fa-file-text-o",
                     "data": "package com.company.project;\n\npublic class HelloWorld {\n\n    public static void main(String[] args) {\n        System.out.println(\"hello world java!\");\n    }\n\n}\n",
                     "cursor": "",
-                    "parent": "71c8eed2-9732-45b4-8299-5eca126bdce3",
+                    "parentId": "71c8eed2-9732-45b4-8299-5eca126bdce3",
                     "children": []
                 }]
             }]
@@ -38,14 +38,14 @@ export var MockResponse:any = {
                 "id": "4078a5de-f759-439d-8f7c-8053db298999",
                 "label": "com.company.project",
                 "icon": "fa fa-th-large",
-                "parent": "765b381c-0bb1-4102-af25-8649d1e7aae8",
+                "parentId": "765b381c-0bb1-4102-af25-8649d1e7aae8",
                 "children": [{
-                    "id": 311,
+                    "id": "d45d9ee5-d049-4e46-8ee4-f6abe6ad6fe6",
                     "label": "HelloWorldTest.java",
                     "icon": "fa-file-text-o",
                     "data": "package com.company.project;\n\nimport org.junit.*;\n\npublic class HelloWorldTest {\n\n    @Test\n    public void test() {\n        Assert.assertEquals(1, 1);\n    }\n}\n",
                     "cursor": "",
-                    "parent": "4078a5de-f759-439d-8f7c-8053db298999",
+                    "parentId": "4078a5de-f759-439d-8f7c-8053db298999",
                     "children": []
                 }]
             }]
