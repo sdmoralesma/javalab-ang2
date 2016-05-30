e5788abbf841ba161a11a744952d5a16a11237c3
Sergio <sdmoralesma@gmail.com>
2016-05-30 23:52:36 +0200

    router upgraded

--- app/boot.ts
+++ app/boot.ts
@@ -1,7 +1,7 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {AppComponent} from "./app.component";
 import "rxjs/Rx";
-import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
+import {ROUTER_PROVIDERS} from "@angular/router";
 import {provide} from "@angular/core";
 import {APP_BASE_HREF} from "@angular/common";
 
