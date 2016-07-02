153dc5a72d71b4f375491aea54bfb0e682d8ade7
Sergio <sdmoralesma@gmail.com>
2016-07-02 16:29:26 +0200

    removed download-split-button and router config (no routes needed by now)

--- app/boot.ts
+++ app/boot.ts
@@ -4,12 +4,10 @@ import "rxjs/Rx";
 import {HTTP_PROVIDERS} from "@angular/http";
 import {TagService} from "./tag.service";
 import {JavalabService} from "./javalab.service";
-import {APP_ROUTER_PROVIDERS} from "./app.routes";
 import {enableProdMode} from "@angular/core";
 
 // enableProdMode();
 bootstrap(AppComponent, [
-    APP_ROUTER_PROVIDERS,
     HTTP_PROVIDERS,
     TagService,
     JavalabService
