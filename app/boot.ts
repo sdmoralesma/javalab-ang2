3e3cde23c17993f04dc2c791395eb479f107cc08
Sergio <sdmoralesma@gmail.com>
2016-07-08 00:20:11 +0200

    added providers

--- app/boot.ts
+++ app/boot.ts
@@ -5,9 +5,11 @@ import {HTTP_PROVIDERS} from "@angular/http";
 import {TagService} from "./tag.service";
 import {JavalabService} from "./javalab.service";
 import {enableProdMode} from "@angular/core";
+import {APP_ROUTER_PROVIDERS} from "./app.routes";
 
 enableProdMode();
 bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS,
     HTTP_PROVIDERS,
     TagService,
     JavalabService
