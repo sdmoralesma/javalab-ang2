ac947260bc88561faeeda03b43a4f759c4f82082
Sergio <sdmoralesma@gmail.com>
2016-07-09 23:04:56 +0200

    integrated hashlocationstrategy

--- app/boot.ts
+++ app/boot.ts
@@ -6,10 +6,12 @@ import {TagService} from "./tag.service";
 import {JavalabService} from "./javalab.service";
 import {enableProdMode} from "@angular/core";
 import {APP_ROUTER_PROVIDERS} from "./app.routes";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 enableProdMode();
 bootstrap(AppComponent, [
     APP_ROUTER_PROVIDERS,
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
     HTTP_PROVIDERS,
     TagService,
     JavalabService
