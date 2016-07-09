92c1b462e7d2e3bb0eb43190cd54484571a2d4ad
Sergio <sdmoralesma@gmail.com>
2016-07-09 23:37:58 +0200

    changed to maincomponent

--- app/boot.ts
+++ app/boot.ts
@@ -1,15 +1,15 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import {AppComponent} from "./app.component";
+import {MainComponent} from "./main.component";
 import "rxjs/Rx";
 import {HTTP_PROVIDERS} from "@angular/http";
 import {TagService} from "./tag.service";
 import {JavalabService} from "./javalab.service";
 import {enableProdMode} from "@angular/core";
-import {APP_ROUTER_PROVIDERS} from "./app.routes";
+import {APP_ROUTER_PROVIDERS} from "./main.routes";
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 enableProdMode();
-bootstrap(AppComponent, [
+bootstrap(MainComponent, [
     APP_ROUTER_PROVIDERS,
     {provide: LocationStrategy, useClass: HashLocationStrategy},
     HTTP_PROVIDERS,
