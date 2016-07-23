0db37af95aa723371186dbfadaf031648e3ee509
Sergio <sdmoralesma@gmail.com>
2016-07-23 18:50:10 +0200

    fixed routes and precompiled class

--- app/main.component.ts
+++ app/main.component.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
+import {AppComponent} from "./app/app.component";
 
 @Component({
     selector: 'javalab-app',
     templateUrl: 'app/main.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    precompile: [AppComponent]
 })
 export class MainComponent {
 
--- app/main.routes.ts
+++ app/main.routes.ts
@@ -2,8 +2,8 @@ import {provideRouter, RouterConfig} from "@angular/router";
 import {AppComponent} from "./app/app.component";
 
 export const routes:RouterConfig = [
-    {path: '/', component: AppComponent},
-    {path: '/:lang', component: AppComponent}
+    {path: '', component: AppComponent},
+    {path: ':lang', component: AppComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
