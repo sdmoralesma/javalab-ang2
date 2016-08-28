e5da6f8d5c61179b5502fe7e41bb88b265fddd17
Sergio <sdmoralesma@gmail.com>
2016-08-28 03:09:00 +0200

    moved services

--- app/services/javalab.service.ts
+++ app/services/javalab.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Http, Headers, RequestOptions} from "@angular/http";
 import "rxjs/add/operator/toPromise";
-import {FileNode, GlobalModel} from "./common";
+import {FileNode, GlobalModel} from "../common";
 
 const BASE = "rest/process";
 
