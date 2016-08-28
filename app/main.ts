e537c8b213a5df1e08e93adf433ee627aea49d71
Sergio <sdmoralesma@gmail.com>
2016-08-28 03:21:34 +0200

    fixed module

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import {AppModule} from "./app.module";
+import {MainModule} from "./main.module";
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(MainModule);
