d6fec104ea42c45d88ef3671f244f9a6c065aa24
Sergio <sdmoralesma@gmail.com>
2016-09-25 21:21:18 +0200

    simplified download method

--- app/services/javalab.service.ts
+++ app/services/javalab.service.ts
@@ -1,17 +1,18 @@
 import {Injectable} from "@angular/core";
-import {Http, Headers, RequestOptions} from "@angular/http";
+import {Http, Headers, RequestOptions, RequestMethod, ResponseContentType} from "@angular/http";
 import "rxjs/add/operator/toPromise";
 import {FileNode, GlobalModel} from "../common";
+import fileSaver = require("file-saver");
 
 const BASE = "http://localhost:48080/rest/process";
 
 @Injectable()
 export class JavalabService {
 
-    constructor(private http:Http) {
+    constructor(private http: Http) {
     }
 
-    initialize(lang:string) {
+    initialize(lang: string) {
         let url = BASE + "/init" + lang;
         return this.http.get(url)
             .toPromise()
@@ -19,12 +20,12 @@ export class JavalabService {
             .catch(this.handleError);
     }
 
-    private handleError(error:any) {
+    private handleError(error: any) {
         console.error("An error occurred:", error);
         return Promise.reject(error.message || error);
     }
 
-    runCode(model:GlobalModel) {
+    runCode(model: GlobalModel) {
         let runCodeURL = BASE + "/run";
         let body = JSON.stringify(model);
         let headers = new Headers({'Content-Type': 'application/json'});
@@ -36,7 +37,7 @@ export class JavalabService {
             .catch(error => this.handleError);
     }
 
-    testCode(model:GlobalModel) {
+    testCode(model: GlobalModel) {
         let testCodeURL = BASE + "/test";
         let body = JSON.stringify(model);
         let headers = new Headers({'Content-Type': 'application/json'});
@@ -49,7 +50,7 @@ export class JavalabService {
     }
 
 
-    findNodeById(id:string, tree:FileNode[]):FileNode {
+    findNodeById(id: string, tree: FileNode[]): FileNode {
         for (var node of tree) {
             var found = this.searchNode(id, node);
             if (found !== null) {
@@ -58,7 +59,7 @@ export class JavalabService {
         }
     }
 
-    private searchNode(id:string, node:FileNode) {
+    private searchNode(id: string, node: FileNode) {
         if (node.id === id) {
             return node;
         } else if (node.children !== null && node.children !== undefined) {
@@ -71,37 +72,17 @@ export class JavalabService {
         return null;
     }
 
-
-    download(model:GlobalModel) {
-        // Xhr creates new context so we need to create reference to this
-        let self = this;
-        var pending:boolean = true;
-
-        // Create the Xhr request object
-        let xhr = new XMLHttpRequest();
-
-        let url = BASE + "/download";
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.responseType = 'blob';
-
-        // Xhr callback when we get a result back
-        // We are not using arrow function because we need the 'this' context
-        xhr.onreadystatechange = function () {
-
-            // We use setTimeout to trigger change detection in Zones
-            setTimeout(() => {
-                pending = false;
-            }, 0);
-
-            // If we get an HTTP status OK (200), save the file using fileSaver
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var blob = new Blob([this.response], {type: 'application/zip'});
-                saveAs(blob, 'project.zip');
+    download(model: GlobalModel) {
+        this.http.post(BASE + "/download", JSON.stringify(model), {
+            method: RequestMethod.Post,
+            responseType: ResponseContentType.Blob,
+            headers: new Headers({'Content-type': 'application/json'})
+        }).subscribe(
+            (response) => {
+                var blob = new Blob([response.blob()], {type: 'application/zip'});
+                var filename = 'test.zip';
+                fileSaver.saveAs(blob, filename);
             }
-        };
-
-        // Start the Ajax request
-        xhr.send(JSON.stringify(model));
+        );
     }
 }
